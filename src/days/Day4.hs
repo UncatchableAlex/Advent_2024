76e432c319b2f21eb6c20d459854dd22ea590796
Alex Meislich <alex.meislich@gmail.com>
2024-12-05 01:48:36 +0100

    massive performance improvements on day 4

--- src/days/Day4.hs
+++ src/days/Day4.hs
@@ -1,34 +1,47 @@
-module DAYS.Day4 (day4, diagonalize, part1, horiz) where 
+module DAYS.Day4 (day4, diagonalize, part1, xmasOnRows) where 
 import Data.List (transpose)
+import Data.IntMap.Strict(IntMap, fromListWith, toList)
 
-horiz :: [[Char]] -> Int
-horiz x = sum $ map horiz' x 
+
+xmasOnRows :: [[Char]] -> Int
+xmasOnRows x = sum $ map xmasOnRows' x 
   where
-    horiz' :: [Char] -> Int
-    horiz' ('X':'M':'A':'S':xs) = 1 + horiz' ('S':xs)
-    horiz' ('S':'A':'M':'X':xs) = 1 + horiz' ('X':xs)
-    horiz' (_:xs) = horiz' xs
-    horiz' [] = 0
+    xmasOnRows' :: [Char] -> Int
+    xmasOnRows' ('X':'M':'A':'S':xs) = 1 + xmasOnRows' ('S':xs)
+    xmasOnRows' ('S':'A':'M':'X':xs) = 1 + xmasOnRows' ('X':xs)
+    xmasOnRows' (_:xs) = xmasOnRows' xs
+    xmasOnRows' [] = 0
 
+-- Fast stateful solution. We hide state with the map, but our result depends on insertion order into the map
 diagonalize :: [[Char]] -> [[Char]]
 diagonalize [] = []
-diagonalize (x:xs) = let n = (length xs + length x) - 1 in map diagonalize' [0..n] where
-  diagonalize' :: Int -> [Char]
-  diagonalize' d = [n | (row, i) <- zip (x:xs) [0..], (n, j) <- zip row [0..], i + j == d]
+diagonalize (x:xs) = map snd $ toList diagMap where
+  diagMap :: IntMap [Char]
+  diagMap = fromListWith (++) diagList
+  diagList :: [(Int, [Char])]
+  diagList = [(i+j, [n]) | (row, i) <- zip (x:xs) [0..], (n, j) <- zip row [0..]]
+
+-- SLOW stateless solution
+-- diagonalize :: [[Char]] -> [[Char]]
+-- diagonalize [] = []
+-- diagonalize (x:xs) = let n = (length xs + length x) - 1 in map diagonalize' [0..n] where
+--   diagonalize' :: Int -> [Char]
+--   diagonalize' d = [n | (row, i) <- zip (x:xs) [0..], (n, j) <- zip row [0..], i + j == d]
+
 
 part1 :: [[Char]] -> Int
 part1 x = forward  + down + diags1 + diags2 where
-  forward = horiz x
-  down = horiz $ transpose x
-  diags1 = horiz $ diagonalize x
-  diags2 = horiz $ diagonalize $ reverse x
+  forward = xmasOnRows x
+  down = xmasOnRows $ transpose x
+  diags1 = xmasOnRows $ diagonalize x
+  diags2 = xmasOnRows $ diagonalize $ reverse x
 
 part2 :: [[Char]] -> Int
 part2 [] = 0
 part2 (x:xs) = sum [isXmas $ box3 i j | i <- [0..m], j <- [0..n]] where
     (m, n) = (length xs - 2, length x - 3)
-    box3 a b = [[y | (y, j) <- zip row [0..length x - 1], j >= b, j < b + 3] 
-      | (row, i) <- zip (x:xs) [0..length xs], i >= a, i < a + 3]
+    box3 :: Int -> Int -> [[Char]]
+    box3 a b = map (take 3 . drop b) $ take 3 $ drop a (x:xs)
 
     isXmas [['M',_,'M'], [_, 'A', _], ['S', _, 'S']] = 1
     isXmas [['M',_,'S'], [_, 'A', _], ['M', _, 'S']] = 1
